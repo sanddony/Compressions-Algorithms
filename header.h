0d7129c293bf9e95860f0fe09e08059e4b8c3165
Sand Donya <sanddony@si-d7.nsk.21-school.ru>
2022-12-06 11:01:51 +0700

    Architicture complete. Added some features

--- header.h
+++ header.h
@@ -1,6 +1,50 @@
 #ifndef HUFFMAN_HEADER_H
 #define HUFFMAN_HEADER_H
+#include <stdio.h>
+#define byte unsigned char
 
+
+static struct option long_options[] = {{"encode", no_argument, NULL, 'e'},
+                                       {"decode", no_argument, NULL, 'd'},
+                                       {"output", required_argument, NULL, 'o'},
+                                       {"input", required_argument, NULL, 'i'},
+                                       {NULL, 0, NULL, 0}};
+
+enum Modes{ENCODE,DECODE};
+
+typedef struct files {
+   FILE* _in;
+   FILE* _out;
+   int mode;
+}files;
+
+typedef struct node node;
+struct node {
+    node* left_leaf;
+    node* right_leaf;
+    int is_root;
+    byte symb;
+    char code;  
+    int weight;
+};
+
+int ParseParams(int argc, char** argv, files* files);
+FILE *GetFile(char *path);
+
+// encode
+int Encode(files files);
+node** SortRoots(node** node_list);
+node** GetFrequencyOfBytes(files files);
+node* BuildTree(node** nodes_list);
+int SerializationOfTheTree(files files, node* root);
+// node* BinarySearch(node* root, char byte);
+int WriteEncodeFile(files files, node* root);
+//
+
+// decode
+node* RestoreTree(files files);
+int WriteDecodeFile(files files, node* root);
+//
 
 
 #endif //HUFFMAN_HEADER_H
--- main.c
+++ main.c
@@ -4,7 +4,162 @@
 #include <unistd.h>
 #include <math.h>
 #include <string.h>
+#include <getopt.h>
+
+
+#include "header.h"
+
+#define BYTE_MAX_SIZE 255
 
 int main(int argc, char** argv) {
 
+    files files = {NULL, NULL, 2};
+    
+    int err_code = ParseParams(argc,argv,&files);
+    if (!err_code) {
+        if(files.mode==ENCODE) {
+          err_code = Encode(files);
+        } else {
+
+        }
+    }
+    return err_code;
+    
+}
+
+int Encode(files files){
+  node** node_list = GetFrequencyOfBytes(files);
+  node* root = BuildTree(node_list);
+}
+
+node** SortRoots(node** node_list){
+  node* tmp;
+  bool noSwap;
+  // Handle NULL case: node can be NULL
+  for (int i = BYTE_MAX_SIZE - 1; i >= 0; i--) {
+        noSwap = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (node_list[j]->weight > node_list[j + 1]->weight)
+            {
+                tmp = node_list[j];
+                node_list[j] = node_list[j + 1];
+                node_list[j + 1] = tmp;
+                noSwap = 0;
+            }
+        }
+        if (noSwap == 1)
+            break;
+  }
+  return node_list;
+}
+
+void F(byte n)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d", ((128 & n)>0));
+        n<<=1;
+    }
+}
+
+node** GetFrequencyOfBytes(files files) {
+  //Define file_size by move the file cursor to the end of the file
+  fseek(files._in, 0, SEEK_END);
+  int file_size = ftell(files._in);
+  fseek(files._in, 0, SEEK_SET);
+  //
+
+  // Allocate mem for mass, which contain 255 elements. Exactly that much, because program encode bytes and BYTE_MAX_SIZE=255 (0x11111111)-uc 
+  node** nodes_list = malloc(sizeof(node*)*BYTE_MAX_SIZE); 
+  for (int i = 0; i < BYTE_MAX_SIZE; i++)
+  {
+    nodes_list[i] = malloc(sizeof(node));
+    nodes_list[i]->symb = i;
+    nodes_list[i]->is_root = 1;
+  }
+  //
+
+  byte tmp[1];
+  for (int i = 0; i < file_size; i++)
+  {
+    fread(tmp, sizeof(byte), 1, files._in);
+    for (int k = 0; k < BYTE_MAX_SIZE; k++)
+    {
+      if(nodes_list[k]->symb==tmp[0]){
+        nodes_list[k]->weight++;
+      }
+    }
+  }
+
+  // for (int i = 0; i < BYTE_MAX_SIZE; i++)
+  // {
+  //   if(nodes_list[i]->weight!=0){
+  //     printf("%d : ",nodes_list[i]->weight);
+  //     F(nodes_list[i]->symb);
+  //     printf("\n");
+  //   }
+  // }
+
+  return SortRoots(nodes_list);
+}
+
+node* BuildTree(node** nodes_list) {
+  node empty_node = {NULL, NULL, 0, NULL, NULL, 0};
+  // while root > 1 in list (or second element not a empty_node)
+  // Take first two elements and build root with them in leafs
+  // Switch first element on the new generated on before step, then 
+  // switch second element on the empty_node
+  
+}
+
+int SerializationOfTheTree(files files, node* root) {
+
+}
+
+int WriteEncodeFile(files files, node* root) {
+
+}
+
+FILE *GetFile(char *path) {
+  FILE *file;
+  if (!strcmp(path, "-")) {
+    file = stdin;
+  } else {
+    file = fopen(path, "rb+");
+  }
+  return file;
+}
+
+int ParseParams(int argc, char** argv, files* files){
+  int err_code = 0;
+  int flag;
+    while ((flag = getopt_long(argc, argv, "edo:i:", long_options, NULL)) !=
+         -1) {
+    switch (flag) {
+      case 'e':
+        (*files).mode = ENCODE;
+        break;
+      case 'd':
+        (*files).mode = DECODE;
+        break;
+      case 'i':
+        (*files)._in = GetFile(optarg);
+        if(!(*files)._in){
+          fprintf(stderr,"No files: %s\n",optarg);
+          err_code = 1;
+        }
+        break;
+      case 'o':
+        (*files)._out = fopen(optarg,"wb+");
+        break;
+      default:
+        break;
+    }
+  }
+  if(!(*files)._in || !(*files)._out || (*files).mode==2){
+      fprintf(stderr,"Usage: haffman [-ed] [-i file_input] [-o file_output]");
+      err_code = 1;
+  }
+  return err_code;
 }
